ec47744d55ae75032029a9274a7a53600bd9b1b2
Anna Oksanichenko <oksanichenkoann@gmail.com>
2020-11-18 18:38:33 +0200

    added elements in viewController and constraints

---
+++ AdvancedWeatherApp/CurrentWeather.swift
@@ -0,0 +1,16 @@
+//
+//  CurrentWeather.swift
+//  AdvancedWeatherApp
+//
+//  Created by Anna Oksanichenko on 18.11.2020.
+//
+
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let tempFeelsLike: Double
+    let pressure: Double
+    let humidity: Double
+    let icon: UIImage
+}
--- AdvancedWeatherApp/ViewController.swift
+++ AdvancedWeatherApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var tempFeelsLikeLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func refreshClicked(_ sender: UIButton) {
+    }
+    
 }
 
